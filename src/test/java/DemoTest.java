bd71b32a713491e4017581e31733f8110316d332
Виктория <viktoriaportnenko@gmail.com>
2025-07-29 20:33:29 +0300

    Initial commit with .gitignore

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,10 +1,17 @@
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class DemoTest {
 
     @Test
 
-    public void seleniumTest(){}
+    public void seleniumTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--start-maximized");
 
-
+    }
 }
